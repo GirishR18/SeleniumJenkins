9c0c0d0972514efb9eef18e6fd5f3b89f43b16cd
girishrayamane18ProOG <girishrayamane18@gmail.com>
2025-09-23 16:01:44 +0530

    Jenkins commit

---
+++ Selenium_project/src/test/java/pack1/ContactTest.java
@@ -0,0 +1,16 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void contactTest() {
+		System.out.println("Contact is getting Created ");
+	}
+	
+	@Test
+	public void contactSearch() {
+		System.out.println("Contact is getting searched ");
+	}
+
+}
---
+++ Selenium_project/src/test/java/pack2/OrgTest.java
@@ -0,0 +1,17 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void OrgCreateTest() {
+		System.out.println("org is getting Created ");
+	}
+	
+	@Test
+	public void orgSearchTest() {
+		System.out.println("org is  searched ");
+	}
+
+}
