844b2ca6d5d9b126c37dacbbf00966573559709f
girishrayamane18ProOG <girishrayamane18@gmail.com>
2025-09-24 15:01:03 +0530

    parameters

--- Selenium_project/src/test/java/pack1/ContactTest.java
+++ Selenium_project/src/test/java/pack1/ContactTest.java
@@ -5,6 +5,17 @@ import org.testng.annotations.Test;
 public class ContactTest {
 	@Test
 	public void contactTest() {
+		String URL = System.getProperty("url");
+		String Browser = System.getProperty("browser");
+		String UserName  = System.getProperty("username");
+		String Password = System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(Browser);
+		
+		System.out.println(UserName);
+		System.out.println(Password);
+		
+		
 		System.out.println("Contact is getting Created ");
 	}
 
